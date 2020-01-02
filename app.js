7ba421fc6a6a3c03b453ac0c2a1d7b8661d678e7
Tianya Liu <tianyaliu1995@gmail.com>
2020-01-02 18:30:12 -0500

    backend

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const expressValidator = require('express-validator');
 require('dotenv').config()
 //import routes
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
+const categoryRoutes = require('./routes/category');
+const productRoutes = require('./routes/product');
 
 //app
 const app = express()
@@ -25,10 +28,14 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(expressValidator());
+app.use(cors());
+
 
 //routes middleware
 app.use('/api', authRoutes);
 app.use('/api', userRoutes);
+app.use('/api', categoryRoutes);
+app.use('/api', productRoutes);
 
 
 const port = process.env.PORT || 8000
--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
 
-const { userById } = require('../controllers/user');
+const { userById, read, update } = require('../controllers/user');
 // const { userById, read, update, purchaseHistory } = require('../controllers/user');
 
 router.get('/secret/:userId', requireSignin, isAuth, isAdmin, (req, res) => {
@@ -12,8 +12,8 @@ router.get('/secret/:userId', requireSignin, isAuth, isAdmin, (req, res) => {
     });
 });
 
-// router.get('/user/:userId', requireSignin, isAuth, read);
-// router.put('/user/:userId', requireSignin, isAuth, update);
+router.get('/user/:userId', requireSignin, isAuth, read);
+router.put('/user/:userId', requireSignin, isAuth, update);
 // router.get('/orders/by/user/:userId', requireSignin, isAuth, purchaseHistory);
 
 //whenever theres a parameter called userID in the route, we execute userById method
